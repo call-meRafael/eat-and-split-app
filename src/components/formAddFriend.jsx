7bc7e4382b4ca03af244a279ec8410af763468e6
countdown-br <raaphael.araujo@gmail.com>
2026-09-14 14:51:41 -0300

    chore(input): swapp hardwrited text for placeholdder property

--- src/components/formAddFriend.jsx
+++ src/components/formAddFriend.jsx
@@ -10,12 +10,12 @@ export const FormAddFriend = () => {
   return (
     <form className="form-add-friend">
       <label htmlFor="friend-name">
-        <UserName  />Nome do amigo</label>
-      <input type="text" name="friend-name" id="iFriend-name" />
+        <UserName  />Usuário</label>
+      <input type="text" name="friend-name" id="iFriend-name" placeholder="Nome do amigo"/>
       <label htmlFor="friend-image">
-        <UserAvatar /> URL da foto{" "}
+        <UserAvatar /> Foto{" "}
       </label>
-      <input type="text" name="image-url" id="iImage-url" />
+      <input type="text" name="image-url" id="iImage-url" placeholder="URL da foto"/>
 
       <Button onClick={handleSubmit}>Adicionar</Button>
     </form>
