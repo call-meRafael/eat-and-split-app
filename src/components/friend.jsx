66b29d8a55cd059ba137840340ec65ca886bbc63
countdown-br <raaphael.araujo@gmail.com>
2026-09-17 09:03:43 -0300

    add a div for friend card info

--- src/components/friend.jsx
+++ src/components/friend.jsx
@@ -6,18 +6,20 @@ export const Friend = ({ friend, formatBalance }) => {
       <div className="avatar-box">
         <img src={friend.image} alt={friend.name} />
       </div>
-        <h3>{friend.name}</h3>
-      {friend.balance < 0 && (
-        <p className="red">
-          Você deve {friend.name}: {formatBalance(Math.abs(friend.balance))}
-        </p>
-      )}
-      {friend.balance > 0 && (
-        <p className="green">
-          {friend.name} deve a você: {formatBalance(Math.abs(friend.balance))}
-        </p>
-      )}
-      {friend.balance === 0 && <p>Você e {friend.name} estão quites.</p>}
+      <div className="firend-info">
+        <h3>{friend.name} {friend.lastName}</h3>
+        {friend.balance < 0 && (
+          <p className="red">
+            Você deve {friend.name}: {formatBalance(Math.abs(friend.balance))}
+          </p>
+        )}
+        {friend.balance > 0 && (
+          <p className="green">
+            {friend.name} deve a você: {formatBalance(Math.abs(friend.balance))}
+          </p>
+        )}
+        {friend.balance === 0 && <p>Você e {friend.name} estão quites.</p>}
+      </div>
       <Button>Selecionar</Button>
     </li>
   );
--- src/data.js
+++ src/data.js
@@ -7,18 +7,21 @@ export const initialFriends = [
   {
     id: 118836,
     name: "Dexter",
+    lastName: "Morgan",
     image: dexterPic,
     balance: -7,
   },
   {
     id: 933372,
     name: "Patrick",
+    lastName: "Jane",
     image: patrickPic,
     balance: 20,
   },
   {
     id: 499476,
     name: "Johan",
+    lastName: "Liebert",
     image: johanPic,
     balance: 0,
   },
