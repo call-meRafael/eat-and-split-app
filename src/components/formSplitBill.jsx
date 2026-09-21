7e607345654760b8b5aa16d89c841a0344541442
countdown-br <raaphael.araujo@gmail.com>
2026-09-21 09:05:02 -0300

    feat(formSplitBill): add an icon to better render the bill

--- src/components/formSplitBill.jsx
+++ src/components/formSplitBill.jsx
@@ -1,7 +1,15 @@
+import { BillIcon } from "./billIcon";
+
+
 export const FormSplitBill =() => {
     return (
         <form className="form-split-bill">
             <h2>Dividir o valor com X</h2>
+            <label><BillIcon />Total da conta</label>
+            <input type="text" name="bill-value" id="iBill-value" placeholder="valor da conta"/>
+
+            <label>Seu gasto</label>
+            <input type="text" name="your-expense" id="iExpense-value" placeholder="insira seu gasto"/>
 
         </form>
     )
