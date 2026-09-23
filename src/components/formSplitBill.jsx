329bd3bdb64cb84b6ff1ff3a507eea934482857d
countdown-br <raaphael.araujo@gmail.com>
2026-09-23 20:39:31 -0300

    chore(formBillSplit): add a class for better control

--- src/components/formSplitBill.jsx
+++ src/components/formSplitBill.jsx
@@ -1,17 +1,33 @@
 import { BillIcon } from "./billIcon";
 import { ExpenseIcon } from "./expenses";
 
+export const FormSplitBill = () => {
+  return (
+    <form className="form-split-bill">
+      <h2>Dividir o valor com X</h2>
 
-export const FormSplitBill =() => {
-    return (
-        <form className="form-split-bill">
-            <h2>Dividir o valor com X</h2>
-            <label><BillIcon />Total da conta</label>
-            <input type="text" name="bill-value" id="iBill-value" placeholder="valor da conta"/>
+      <label className="bill-value">
+        <BillIcon />
+        Total da conta
+      </label>
 
-            <label><ExpenseIcon />Seu gasto</label>
-            <input type="text" name="your-expense" id="iExpense-value" placeholder="insira seu gasto"/>
+      <input
+        type="text"
+        name="bill-value"
+        id="iBill-value"
+        placeholder="valor da conta"
+      />
 
-        </form>
-    )
-}
+      <label className="bill-value">
+        <ExpenseIcon />
+        Seu gasto
+      </label>
+      <input
+        type="text"
+        name="your-expense"
+        id="iExpense-value"
+        placeholder="insira seu gasto"
+      />
+    </form>
+  );
+};
